d8844dd9c3fe1e2c8a84d52b550dc0d6f6374ade
Brandon Brown <brandon@brandonb.ca>
2024-03-03 15:20:55 -0800

    Fix lint and test workflow, adds use of dotenv for env var loading

--- src/utils/loadConfig.js
+++ src/utils/loadConfig.js
@@ -1,4 +1,4 @@
-import fsExtra from 'fs-extra';
+import 'dotenv/config';
 
 function getEnv(name, fallback = null) {
   let val = process.env[name];
@@ -11,26 +11,16 @@ function getEnv(name, fallback = null) {
   return val;
 }
 
-const jsonFile = new URL('../../config.json', import.meta.url);
-let config;
-
-// Support config.json for config values
-if (fsExtra.existsSync(jsonFile)) {
-  config = await fsExtra.readJson(jsonFile);
-} else {
-  // Allow environment variables to be defined as well for config values
-  config = {
-    token: getEnv('BOT_DISCORD_TOKEN'),
-    prefix: getEnv('BOT_MSG_PREFIX', '?'),
-    aprs_token: getEnv('BOT_APRSFI_TOKEN'),
-    gmaps_token: getEnv('BOT_GMAPS_TOKEN'),
-    timezone: getEnv('BOT_TIMEZONE', 'America/Vancouver'),
-    embed_color: getEnv('BOT_EMBED_COLOR', '#efefef'),
-    user_agent: getEnv(
-      'BOT_USER_AGENT',
-      'amateur-radio-discord-bot v1 (+https://github.com/brandonb927/amateur-radio-discord-bot)'
-    ),
-  };
-}
-
-export default config;
+// Allow environment variables to be defined as well for config values
+export default {
+  token: getEnv('BOT_DISCORD_TOKEN'),
+  prefix: getEnv('BOT_MSG_PREFIX', '?'),
+  aprs_token: getEnv('BOT_APRSFI_TOKEN'),
+  gmaps_token: getEnv('BOT_GMAPS_TOKEN'),
+  timezone: getEnv('BOT_TIMEZONE', 'America/Vancouver'),
+  embed_color: getEnv('BOT_EMBED_COLOR', '#efefef'),
+  user_agent: getEnv(
+    'BOT_USER_AGENT',
+    'amateur-radio-discord-bot v1 (+https://github.com/brandonb927/amateur-radio-discord-bot)'
+  ),
+};
