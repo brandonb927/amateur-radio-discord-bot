679ebc2610ba5c7de6891443b0f89e409b076ee6
Brandon Brown <brandon@brandonb.ca>
2024-04-19 16:52:59 -0700

    Clean up admin role

--- src/utils/roles.js
+++ src/utils/roles.js
@@ -1,5 +1,9 @@
 import { User } from 'discord.js';
 
+export class Role {
+  BOT_ADMIN = 'HamBot Admin';
+}
+
 /**
  * Returns whether or not a given user has a specific "admin" role
  *
@@ -7,6 +11,6 @@ import { User } from 'discord.js';
  * @param {string} role Discord role
  * @returns {boolean}
  */
-export function hasRole(user, role = 'Bot Admin') {
+export function hasRole(user, role = Role.BOT_ADMIN) {
   return user.roles.cache.some((r) => r.name === role);
 }
